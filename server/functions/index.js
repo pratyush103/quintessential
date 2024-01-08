07325c9b7cbfb6de8802db2975b8c92532767f74
Pratyush Landekar <pratyushlandekar103@gmail.com>
2024-01-09 01:54:56 +0530

    First endpoint employed to test the server

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,42 +1,20 @@
 /* eslint-disable no-unused-vars */
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-//  https://blog.postman.com/how-to-create-a-rest-api-with-node-js-and-express/
-
-// The Cloud Functions for Firebase SDK to create Cloud Functions and triggers.
-
-const {onRequest} = require("firebase-functions/v2/https");
-const {logger} = require("firebase-functions");
-const {onDocumentCreated} = require("firebase-functions/v2/firestore");
-
-// The Firebase Admin SDK to access Firestore.
-const {initializeApp} = require("firebase-admin/app");
-const {getFirestore} = require("firebase-admin/firestore");
-
-// Import the functions you need from the SDKs you need
-const {getAuth,
-  connectAuthEmulator,
-  signInWithEmailAndPassword} = require("firebase-admin/auth");
-const {getAnalytics} = require("firebase/analytics");
-
-// Express Setup
+/*  eslint linebreak-style: ["error", "windows"]*/
 const express = require("express");
+const {signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  getAuth,
+  connectAuthEmulator} = require("firebase/auth");
+
+// Initialize Firebase
+const {initializeApp} = require("firebase/app");
+const {getFirestore} = require("firebase/firestore");
+
 const app = express();
 const PORT = process.env.PORT || 8080;
 app.use(express.json());
 
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyALza1jqAvS7YLHWqMwNHycGwNytNaLNSc",
   authDomain: "quintessential-6c53e.firebaseapp.com",
@@ -48,38 +26,36 @@ const firebaseConfig = {
   measurementId: "G-RFVFRJW6RD",
 };
 
+
 // Initialize Firebase
 const fireApp = initializeApp(firebaseConfig);
-const analytics = getAnalytics(fireApp);
 const db = getFirestore(fireApp);
 const auth = getAuth(fireApp);
 
+// Connect to the Firebase Authentication emulator if running locally
 connectAuthEmulator(auth, "http://localhost:9099");
 
-// Firebase Methods
+// Express endpoint for user sign-up
+app.post("/signup-email", (req, res) => {
+  const {email, password} = req.body;
 
-fireApp.auth().createUserWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-    // Signed in
-      const user = userCredential.user;
-    // ...
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-    // ..
-    });
+  // Sign up the user with email and password
+  createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        res.json(user); // Send the user details as JSON response
+        console.log(userCredential.user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        res.status(400).json({errorCode, errorMessage});
+        // Send error details as JSON response
+      });
+});
 
-
-// Express Methods and enpoints
+// Start the Express server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
